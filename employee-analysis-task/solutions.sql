7952c35d813dbd427d72b68cdb5b24e72db92772
Rixongile98 <malulekexongilejudy@gmail.com>
2025-11-11 15:14:38 +0200

    added screenshots

---
+++ employee-analysis-task/solutions.sql
@@ -0,0 +1,22 @@
+Question 1
+
+SELECT *
+FROM employees
+WHERE department = 'HR';
+
+Question 2
+
+SELECT *
+FROM employees
+WHERE salary > 80000;
+
+Question 3
+
+SELECT *
+FROM employees
+WHERE hire_date > '2019-01-01';
+
+Question 4
+
+DELETE FROM employees
+WHERE department = 'Sales';
